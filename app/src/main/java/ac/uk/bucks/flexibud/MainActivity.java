bcb6a37a1fd11c991437b62baca12e89f8ee51d7
Matt Enfield <44966386+mattenfield@users.noreply.github.com>
2020-01-05 14:45:22 +0000

    added registration and sign in functionality from initial screen.

--- app/src/main/java/ac/uk/bucks/flexibud/MainActivity.java
+++ app/src/main/java/ac/uk/bucks/flexibud/MainActivity.java
@@ -1,15 +1,95 @@
 package ac.uk.bucks.flexibud;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
+    ProgressBar progressBar;
+    EditText email;
+    EditText password;
+    Button signin;
+    Button signup;
+
+    FirebaseAuth firebaseAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.splash_screen);
+        setContentView(R.layout.activity_main);
+
+        progressBar = findViewById(R.id.progressBar);
+        email = findViewById(R.id.etEmail);
+        password = findViewById(R.id.etPassword);
+        signin = findViewById(R.id.btnSignIn);
+        signup = findViewById(R.id.btnSignUp);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                progressBar.setVisibility(View.VISIBLE);
+                firebaseAuth.createUserWithEmailAndPassword(email.getText().toString(),
+                        password.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                progressBar.setVisibility(View.GONE);
+
+                                if(task.isSuccessful()){
+                                    Toast.makeText(MainActivity.this, "Registered Successfully", Toast.LENGTH_LONG).show();
+                                }
+                                else{
+                                    Toast.makeText(MainActivity.this, task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                                }
+
+
+                            }
+
+                        });
+
+            }
+        });
+
+        signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                progressBar.setVisibility(View.VISIBLE);
+                firebaseAuth.signInWithEmailAndPassword(email.getText().toString(),
+                        password.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>(){
+
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        progressBar.setVisibility(View.GONE);
+
+                        if(task.isSuccessful()){
+                            setContentView(R.layout.main_menu);
+                            Toast.makeText(MainActivity.this, "Login Successful", Toast.LENGTH_LONG).show();
+
+                        }
+                        else{
+                            Toast.makeText(MainActivity.this, task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                        }
+
+
+                    }
+
+                });
+
+            }
+        });
 
     }
 }
